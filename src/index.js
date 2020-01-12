b4e4a43d9bec678f7aa6b258282b21fe6d55f3be
linda413 <caiqinli99@yahoo.com.tw>
2020-01-11 18:08:44 -0800

    Chatroom Basic Finish!

--- src/index.js
+++ src/index.js
@@ -13,12 +13,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const hbs = require('hbs')
 const cors = require('cors');
-const Chatkit = require('@pusher/chatkit-server');
 
-const chatkit = new Chatkit.default({
-    instanceLocator: process.env.CHATKIT_INSTANCE_LOCATOR,
-    key: process.env.CHATKIT_SECRET_KEY,
-});
 
 // Paths
 const publicDirectoryPath = path.join(__dirname, 'templates/public')
@@ -49,7 +44,44 @@ app.use(applicantsRouter)
 app.use(appRouter)
 app.use(pusherRouter)
 
+const Chatkit = require('@pusher/chatkit-server');
 
+const chatkit = new Chatkit.default({
+    instanceLocator: process.env.CHATKIT_INSTANCE_LOCATOR,
+    key: process.env.CHATKIT_SECRET_KEY,
+});
+
+app.post('/users', (req, res) => {
+    const { userId } = req.body;
+    chatkit
+      .createUser({
+        id: userId,
+        name: userId,
+      })
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch(err => {
+        if (err.error === 'services/chatkit/user_already_exists') {
+          console.log(`User already exists: ${userId}`);
+          res.sendStatus(200);
+        } else {
+          res.status(err.status).json(err);
+        }
+    });
+});
+
+/* while /authenticate validates all incoming connections to our Chatkit instance by responding with a token (returned by chatkit.
+authenticate) if the request is valid. For the purpose of this tutorial, 
+we don’t actually try to validate the request before returning the token, 
+but you need to do so in your production code. */
+
+app.post('/authenticate', (req, res) => {
+    const authData = chatkit.authenticate({
+      userId: req.query.user_id,
+    });
+    res.status(authData.status).send(authData.body);
+});
 
 app.listen(port, () => {
     console.log('Server is up on port', port);
--- src/templates/public/src/chatroom.jsx
+++ src/templates/public/src/chatroom.jsx
@@ -1,62 +1,111 @@
-import React, { Component } from 'react';
+ // client/src/App.js
 
+import React, { Component } from 'react';
+import {
+  handleInput,
+  connectToChatkit,
+  connectToRoom,
+  sendMessage, 
+  sendDM,
+} from './methods';
+import RoomList from './components/roomList';
+import ChatSession from './components/chatSession';
+import Dialog from './components/dialog';
 import 'skeleton-css/css/normalize.css';
 import 'skeleton-css/css/skeleton.css';
 import '../chatroom.css';
+import RoomUsers from './components/roomUsers';
 
-class Chat extends Component {
-  constructor() {
-    console.log("construct chat")
-    super();
-    this.state = {
-      userId: '',
-      showLogin: true,
-      isLoading: false,
-      currentUser: null,
-      currentRoom: null,
-      rooms: [],
-      roomUsers: [],
-      roomName: null,
-      messages: [],
-      newMessage: '',
-    };
+class App extends Component {
+   constructor() {
+     super();
+     this.state = {
+       userId: '',
+       showLogin: true,
+       isLoading: false,
+       currentUser: null,
+       currentRoom: null,
+       rooms: [],
+       roomUsers: [],
+       roomName: null,
+       messages: [],
+       newMessage: '',
+     };
+     this.handleInput = handleInput.bind(this);
+     this.connectToChatkit = connectToChatkit.bind(this);
+     this.connectToRoom = connectToRoom.bind(this);
+     this.sendMessage = sendMessage.bind(this);
+     this.sendDM = sendDM.bind(this);
+   }
 
-  }
-
-  render() {
-    const {
-      userId,
-      showLogin,
-      rooms,
-      currentRoom,
-      currentUser,
-      messages,
-      newMessage,
-      roomUsers,
-      roomName,
-    } = this.state;
+    render() {
+      const {
+       userId,
+       showLogin,
+       rooms,
+       currentRoom,
+       currentUser,
+       messages,
+       newMessage,
+       roomUsers,
+       roomName,
+      } = this.state;
 
     return (
       <div className="App">
-        <aside className="sidebar left-sidebar"></aside>
+        <aside className="sidebar left-sidebar">
+              {currentUser ? (
+                <div className="user-profile">
+                  <span className="user-id">{`@${currentUser.id}`}</span>
+                </div>
+              ) : null}
+              {currentRoom ? (
+                <RoomList
+                  rooms={rooms}
+                  currentRoom={currentRoom}
+                  connectToRoom={this.connectToRoom}
+                  currentUser={currentUser}
+                />
+              ) : null}
+        </aside>
         <section className="chat-screen">
-          <header className="chat-header"></header>
-          <ul className="chat-messages"></ul>
-          <footer className="chat-footer">
-            <form className="message-form">
-              <input
-                type="text"
-                name="newMessage"
-                className="message-input"
-                placeholder="Type your message and hit ENTER to send"
-              />
-            </form>
-          </footer>
+              <header className="chat-header">
+                {currentRoom ? <h3>{roomName}</h3> : null}
+              </header>
+              <ul className="chat-messages">
+                <ChatSession messages={messages} />
+              </ul>
+              <footer className="chat-footer">
+                <form onSubmit={this.sendMessage} className="message-form">
+                  <input
+                    type="text"
+                    value={newMessage}
+                    name="newMessage"
+                    className="message-input"
+                    placeholder="Type your message and hit ENTER to send"
+                    onChange={this.handleInput}
+                  />
+                </form>
+              </footer>
         </section>
-        <aside className="sidebar right-sidebar"></aside>
-      </div>
-    );
-  }
-}
+        <aside className="sidebar right-sidebar">
+              {currentRoom ? (
+                <RoomUsers
+                  currentUser={currentUser}
+                  roomUsers={roomUsers}
+                />
+              ) : null}
+        </aside>
+        {showLogin ? (
+          <Dialog
+            userId={userId}
+            handleInput={this.handleInput}
+            connectToChatkit={this.connectToChatkit}
+          />
+        ) : null}
+        </div>
+     );
+   }
+ }
 
-export default Chat;
+ export default App;
